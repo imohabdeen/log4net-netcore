0ccd44099b7b296ca68cc9fb75792e156ad89ebc
Mohamed Abdeen <mohabdeen@etisalat.ae>
2018-04-13 11:04:43 +0400

    Initial log4net demo for .Net core with basic info logger for console appender

---
+++ Log4netNetCoreDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Log4netNetCoreDemo
+{
+    class Program
+    {
+        private static readonly log4net.ILog log = log4net.LogManager
+            .GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+
+        static void Main(string[] args)
+        {
+            var logRepository = log4net.LogManager.GetRepository(System.Reflection.Assembly.GetEntryAssembly());
+
+            //log4net configuraiton for consoleappendar - no config gile needed
+            //log4net.Config.BasicConfigurator.Configure(logRepository);
+
+            //Load configuration from log4net.config file
+            log4net.Config.XmlConfigurator.Configure(logRepository,
+                                                     new System.IO.FileInfo("log4net.config"));
+
+
+            Console.WriteLine("Hello World!");
+
+            log.Info("Log4net for .Net Core Hello World");
+        }
+    }
+}
